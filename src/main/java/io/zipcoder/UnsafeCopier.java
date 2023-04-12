68602356905a7ac54b13e534281d9326d28a84fb
Jacob C <jacobc@Zipcoders-MacBook-Pro.local>
2023-04-12 17:43:39 -0400

    done

--- src/main/java/io/zipcoder/UnsafeCopier.java
+++ src/main/java/io/zipcoder/UnsafeCopier.java
@@ -1,14 +1,26 @@
 package io.zipcoder;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Modify the run function so that the monkeys each grab the next word and write it to the copy.
  */
 public class UnsafeCopier extends Copier {
 
+
     public UnsafeCopier(String toCopy) {
         super(toCopy);
+    
     }
 
     public void run() {
+     //   StringBuilder s = new StringBuilder();
+        while(stringIterator.hasNext()){
+            copied += stringIterator.next() + " ";
+         //  s.append(stringIterator.next()).append(" ");
+        }
+       // copied = s.toString();
+        //System.out.println(copied);
+
     }
 }
